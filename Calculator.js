023e055825ae81b4e122d72c1611013bde79c753
Avichai Shapira <88307681+avichai-sealights@users.noreply.github.com>
2022-07-18 11:26:36 +0300

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -14,6 +14,7 @@ module.exports = {
     
         for (var i = 1; i < arguments.length; i++) {
             substraction -= arguments[i];
+            substraction += 0;
         }
 
         return substraction + arguments[0];
