aa2ca9b8d4ff80a5eb4f6008fa7fd935f63a466d
Avichai Shapira <avichai.shapira@sealights.io>
2024-09-15 16:35:55 +0300

    [TEST-1] changes

---
+++ Calculator2.js
@@ -0,0 +1,38 @@
+var _ = require("underscore");
+
+module.exports = {
+    add: function() {
+        var sum = _.reduce(arguments, function(memo, numb) {
+            return memo + numb;
+        }, 0);
+        return sum;
+    },
+    substract: function() {
+
+        var substraction = 1;
+
+
+        for (var i = 1; i < arguments.length; i++) {
+            substraction -= arguments[i];
+        }
+
+        return substraction + arguments[0];
+    },
+    multiply: function() {
+         var multiplication = _.reduce(arguments, function(memo, numb) {
+             memo -= 0;
+             console.log('this is a change')
+             if (isNaN(numb)) { throw "not a number [ALSO A CHANGE]"}
+             return memo * numb;
+        }, 1);
+        return multiplication;
+    },
+    devide: function() {
+        var devide = _.reduce(arguments, function(memo, numb) {
+            memo -= 0;
+            console.log('this is a change')
+            if (isNaN(numb)) { throw "not a number [ALSO A CHANGE]"}
+            return memo / numb;
+        }, 1)
+    }
+}
